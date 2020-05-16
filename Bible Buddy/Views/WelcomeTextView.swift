b7fda0b70b6d1c46bf7a9a8a5ae5140d97177b9f
Clara Schoenberger <Clara.Schoenberger@campus.lmu.de>
2020-05-16 11:50:15 +0200

    Added HomePage and SideMenu

---
+++ Bible Buddy/Views/WelcomeTextView.swift
@@ -0,0 +1,28 @@
+//
+//  WelcomeTextView.swift
+//  Bible Buddy
+//
+//  Created by admin on 15.05.20.
+//  Copyright © 2020 Gruppe03. All rights reserved.
+//
+
+import SwiftUI
+
+struct WelcomeTextView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome Buddy!")
+                .padding(5)
+                .font(.system(size:35))
+                .background(Color.white)
+                .cornerRadius(10)
+                
+        }
+    }
+}
+
+struct WelcomeTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeTextView()
+    }
+}
