c87c94c8a75e14d45d39eec34bac10cc4bdbe514
James Min <Jamesmin90@gmail.com>
2020-06-27 14:44:22 +0200

    fixing chatroom

--- Bible Buddy/Views/SignInUpView.swift
+++ Bible Buddy/Views/SignInUpView.swift
@@ -59,34 +59,31 @@ struct SignInUpView: View {
     }
     
     func signIn() {
-        let storage = Storage.storage().reference()
-        
-        let uid = Auth.auth().currentUser?.uid
-        
         session.signIn(email: email, password: password) {
             (result, error) in
-            self.signInUpCompletionHandler(error: error)
-        }
-        
-        storage.child("Profiles").child(uid!).downloadURL { (url, err) in
-        
-        if err != nil{
             
-            print((err?.localizedDescription)!)
-            return
-        }
-        
-        checkUser { (exists, userName, uid, url) in
-            if exists{
-                UserDefaults.standard.set(true, forKey: "status")
-                UserDefaults.standard.set(userName, forKey: "userName")
-                UserDefaults.standard.set(uid, forKey: "uid")
-                UserDefaults.standard.set("\(url)", forKey: "pic")
-                NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
+            if error != nil{
+            self.signInUpCompletionHandler(error: error)
+            }
+            checkUser { (exists, userName, uid, pic) in
+                
+                if exists{
+                     UserDefaults.standard.set(true, forKey: "status")
+                     
+                     UserDefaults.standard.set(userName, forKey: "userName")
+                    
+                     UserDefaults.standard.set(uid, forKey: "uid")
+                    
+                     UserDefaults.standard.set(pic, forKey: "pic")
+                     
+                     NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
+                }
+                else{
+                    self.loading.toggle()
+                    self.creation.toggle()
+                }
             }
         }
-        }
-        
     }
     
     func signUp() {
