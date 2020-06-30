ec2d4ffcfcb4cf977afdc380bd173cf9850ec48c
James Min <Jamesmin90@gmail.com>
2020-06-30 12:22:33 +0200

    fixing keyboard

--- Bible Buddy/AppDelegate.swift
+++ Bible Buddy/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -23,6 +24,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         appearance.largeTitleTextAttributes = attrs
         UINavigationBar.appearance().scrollEdgeAppearance = appearance
         FirebaseApp.configure()
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
         return true
     }
 
